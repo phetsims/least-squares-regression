8d8cd478e6dfacf9b4a95b764fdf69d1be0b3c2f
bereaphysics <martin.veillette@gmail.com>
2014-10-27 23:05:02 -0600

    included a text equation that  links to the slope and intercept property

--- js/least-squares-regression/view/MyLineBoxNode.js
+++ js/least-squares-regression/view/MyLineBoxNode.js
@@ -16,6 +16,7 @@ define( function( require ) {
   var Panel = require( 'SUN/Panel' );
   // var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Range = require( 'DOT/Range' );
+  var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
   var Text = require( 'SCENERY/nodes/Text' );
   var Util = require( 'DOT/Util' );
   var VBox = require( 'SCENERY/nodes/VBox' );
@@ -32,7 +33,7 @@ define( function( require ) {
   // constants
   // var FONT = new PhetFont( 11 );
 
-
+  var pattern_0slope_1intercept = " y = {0} x + {1} ";
   /**
    * {Model} model of the main simulation
    * {Object} options
@@ -40,17 +41,16 @@ define( function( require ) {
    */
   function MyLineBoxNode( model, options ) {
 
-    var slopeText = new Text( 'a', { fill: 'white', stroke: 'black', cornerRadius: 2 } );
-    var interceptText = new Text( 'b', { fill: 'white', stroke: 'black', cornerRadius: 2 } );
 
-    var equationPanel = new Panel( slopeText );
+    var equationText = new Text( '', { fill: 'white', stroke: 'black', cornerRadius: 2 } );
+
     Panel.call( this, new VBox( {spacing: 5, children: [
         new CheckBox( new Text( myLineString ), model.showMyLineProperty ),
-        equationPanel,
+        new Panel( equationText ),
         new Text( 'y= a x + b' ),
         new HBox( {spacing: 5, children: [
-          new VerticalSlider( aString, new Dimension2( 10, 100 ), model.interceptProperty, new Range( 0, 20 ) ),
-          new VerticalSlider( bString, new Dimension2( 10, 100 ), model.slopeProperty, new Range( 0, 20 ) )]} ),
+          new VerticalSlider( aString, new Dimension2( 10, 100 ), model.slopeProperty, new Range( -20, 20 ) ),
+          new VerticalSlider( bString, new Dimension2( 10, 100 ), model.interceptProperty, new Range( -20, 20 ) )]} ),
         new CheckBox( new Text( residualsString ), model.showResidualsOfMyLineProperty ),
         new CheckBox( new Text( squaredResidualsString ), model.showSquareResidualsOfMyLineProperty )
       ], align: 'left' } ),
@@ -67,15 +67,19 @@ define( function( require ) {
       }, options )
     );
 
-
+    var interceptText;
+    var slopeText;
+    var equationText;
     // move the slider thumb to reflect the model value
     model.interceptProperty.link( function( intercept ) {
-      interceptText.text = Util.toFixedNumber( intercept, 2 );
+      interceptText = Util.toFixedNumber( intercept, 1 );
+      equationText.text = StringUtils.format( pattern_0slope_1intercept, slopeText, interceptText );
     } );
 
     // move the slider thumb to reflect the model value
     model.slopeProperty.link( function( slope ) {
-      slopeText.text = Util.toFixedNumber( slope, 2 );
+      slopeText = Util.toFixedNumber( slope, 1 );
+      equationText.text = StringUtils.format( pattern_0slope_1intercept, slopeText, interceptText );
     } );
 
   }
