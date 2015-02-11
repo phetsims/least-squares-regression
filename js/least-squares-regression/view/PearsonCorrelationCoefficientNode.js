b4bd69ca3732e89da332bdac5d31bd62986978a2
bereaphysics <martin.veillette@gmail.com>
2015-02-10 18:50:41 -0700

    add accordion Box to the Pearson Correlation Coefficient

--- js/least-squares-regression/view/PearsonCorrelationCoefficientNode.js
+++ js/least-squares-regression/view/PearsonCorrelationCoefficientNode.js
@@ -9,11 +9,13 @@ define( function( require ) {
   'use strict';
 
   // modules
+  var AccordionBox = require( 'SUN/AccordionBox' );
   var HStrut = require( 'SUN/HStrut' );
   var inherit = require( 'PHET_CORE/inherit' );
   var LSRConstants = require( 'LEAST_SQUARES_REGRESSION/least-squares-regression/LeastSquaresRegressionConstants' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Panel = require( 'SUN/Panel' );
+  var Property = require( 'AXON/Property' );
   var StringUtils = require( 'PHETCOMMON/util/StringUtils' );
   var Text = require( 'SCENERY/nodes/Text' );
   var Util = require( 'DOT/Util' );
@@ -23,6 +25,7 @@ define( function( require ) {
   var pattern_0r_1value = "{0} {1}";
   var plusString = '\u002B'; // we want a large + sign
   var minusString = '\u2212';
+  var correlationCoefficientString = require( 'string!LEAST_SQUARES_REGRESSION/correlationCoefficient' );
 
   /**
    *
@@ -31,18 +34,25 @@ define( function( require ) {
    */
   function PearsonCorrelationCoefficientNode( graph ) {
 
-    Node.call( this );
+    // property of the accordion Box
+    this.expandedProperty = new Property( false );
+
     this.graph = graph;
 
+    // Create the left hand side of the equation (includes the equal sign)
     var leftHandSideText = new Text( rEqualsString, { font: LSRConstants.PEARSON_COEFFICIENT_TEXT_FONT } );
+
+    // Create the right hand side of the equation
     this.rightHandSideText = new Text( '', { font: LSRConstants.PEARSON_COEFFICIENT_TEXT_FONT } );
 
+    // calculate the maximum width of the right hand side of the equation
     var rightHandSideMaxWidth = new Text( plusString + ' 0.00', { font: LSRConstants.PEARSON_COEFFICIENT_TEXT_FONT } ).width;
     var hStrut = new HStrut( rightHandSideMaxWidth );
 
     hStrut.left = leftHandSideText.right + 5;
     this.rightHandSideText.left = leftHandSideText.right + 5;
 
+    // Create the equation
     var equation = new Node( {
       children: [
         leftHandSideText,
@@ -51,21 +61,42 @@ define( function( require ) {
       ]
     } );
 
-    var mutableEquationText = new Panel( equation, {
+    // Create the panel that holds the equation
+    var mutableEquationPanel = new Panel( equation, {
       fill: LSRConstants.GRAPH_BACKGROUND_COLOR,
       cornerRadius: LSRConstants.SMALL_PANEL_CORNER_RADIUS,
       resize: false,
       xMargin: 10
     } );
 
-    this.addChild( mutableEquationText );
+    // Options for the Accordion Box
+    var options = _.extend( {
+      buttonXMargin: 10,
+      buttonYMargin: 10,
+      expandedProperty: this.expandedProperty,
+      titleNode: new Text( correlationCoefficientString, { font: LSRConstants.TEXT_FONT_BOLD } ),
+      titleXMargin: 30,
+      contentXMargin: 10,
+      contentYMargin: 10
+    }, options );
+
+    // Add the panel to the Accordion Box
+    AccordionBox.call( this, mutableEquationPanel,
+      options );
   }
 
-  return inherit( Node, PearsonCorrelationCoefficientNode, {
+  return inherit( AccordionBox, PearsonCorrelationCoefficientNode, {
     reset: function() {
+      // Close the accordion Box
+      this.expandedProperty.reset();
+      // Update the text of the equation
       this.update();
     },
 
+    /**
+     * Update the value of the right hand side of  the equation
+     * @public
+     */
     update: function() {
       var rValueString;
       // Check for the existence of the rValue
