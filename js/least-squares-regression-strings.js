e9e36adfa02e6693e6b0c85c00d63e72f8caae93
samreid <reids@colorado.edu>
2020-02-27 12:07:56 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/least-squares-regression-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import leastSquaresRegression from './leastSquaresRegression.js';
+
+const leastSquaresRegressionStrings = getStringModule( 'LEAST_SQUARES_REGRESSION' );
+
+leastSquaresRegression.register( 'leastSquaresRegressionStrings', leastSquaresRegressionStrings );
+
+export default leastSquaresRegressionStrings;
