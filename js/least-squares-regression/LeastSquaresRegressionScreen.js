b9e8c894dca2c6efed9737bdaa2eadab6803cdff
pixelzoom <cmalley@pixelzoom.com>
2020-09-25 15:43:29 -0600

    convert Screen subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/least-squares-regression/LeastSquaresRegressionScreen.js
+++ js/least-squares-regression/LeastSquaresRegressionScreen.js
@@ -7,24 +7,20 @@
 
 import Property from '../../../axon/js/Property.js';
 import Screen from '../../../joist/js/Screen.js';
-import inherit from '../../../phet-core/js/inherit.js';
 import leastSquaresRegression from '../leastSquaresRegression.js';
 import LeastSquaresRegressionConstants from './LeastSquaresRegressionConstants.js';
 import LeastSquaresRegressionModel from './model/LeastSquaresRegressionModel.js';
 import LeastSquaresRegressionScreenView from './view/LeastSquaresRegressionScreenView.js';
 
-/**
- * @constructor
- */
-function LeastSquaresRegressionScreen() {
-  Screen.call( this,
-    function() { return new LeastSquaresRegressionModel(); },
-    function( model ) { return new LeastSquaresRegressionScreenView( model ); },
-    { backgroundColorProperty: new Property( LeastSquaresRegressionConstants.BACKGROUND_COLOR ) }
-  );
+class LeastSquaresRegressionScreen extends Screen {
+  constructor() {
+    super(
+      function() { return new LeastSquaresRegressionModel(); },
+      function( model ) { return new LeastSquaresRegressionScreenView( model ); },
+      { backgroundColorProperty: new Property( LeastSquaresRegressionConstants.BACKGROUND_COLOR ) }
+    );
+  }
 }
 
 leastSquaresRegression.register( 'LeastSquaresRegressionScreen', LeastSquaresRegressionScreen );
-
-inherit( Screen, LeastSquaresRegressionScreen );
 export default LeastSquaresRegressionScreen;
