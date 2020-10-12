7b728f458c9211a2ad18e9a07be57a77686d45cf
pixelzoom <cmalley@pixelzoom.com>
2020-10-12 15:49:15 -0600

    convert ScreenView subclasses to ES6 subclass, https://github.com/phetsims/tasks/issues/1044

--- js/least-squares-regression/view/SourceAndReferenceNode.js
+++ js/least-squares-regression/view/SourceAndReferenceNode.js
@@ -8,7 +8,6 @@
 
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import ScreenView from '../../../../joist/js/ScreenView.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import MultiLineText from '../../../../scenery-phet/js/MultiLineText.js';
 import Circle from '../../../../scenery/js/nodes/Circle.js';
@@ -16,100 +15,100 @@ import LayoutBox from '../../../../scenery/js/nodes/LayoutBox.js';
 import Line from '../../../../scenery/js/nodes/Line.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import Panel from '../../../../sun/js/Panel.js';
-import leastSquaresRegressionStrings from '../../leastSquaresRegressionStrings.js';
 import leastSquaresRegression from '../../leastSquaresRegression.js';
+import leastSquaresRegressionStrings from '../../leastSquaresRegressionStrings.js';
 import LeastSquaresRegressionConstants from '../LeastSquaresRegressionConstants.js';
 
 const sourcePatternString = leastSquaresRegressionStrings.sourcePattern;
 
-/**
- * @param {Property.<DataSet>} selectedDataSetProperty
- * @constructor
- */
-function SourceAndReferenceNode( selectedDataSetProperty ) {
+class SourceAndReferenceNode extends ScreenView {
 
-  /*
-   * Use ScreenView, to help center and scale content. Renderer must be specified here because the window is added
-   * directly to the scene, instead of to some other node that already has svg renderer.
+  /**
+   * @param {Property.<DataSet>} selectedDataSetProperty
    */
-  ScreenView.call( this, { layoutBounds: new Bounds2( 0, 0, 1024, 618 ) } );
+  constructor( selectedDataSetProperty ) {
 
-  // limit the width of the dialog content for i18n
-  const maxContentWidth = this.layoutBounds.width * 2 / 3;
+    /*
+     * Use ScreenView, to help center and scale content. Renderer must be specified here because the window is added
+     * directly to the scene, instead of to some other node that already has svg renderer.
+     */
+    super( { layoutBounds: new Bounds2( 0, 0, 1024, 618 ) } );
 
-  const self = this;
+    // limit the width of the dialog content for i18n
+    const maxContentWidth = this.layoutBounds.width * 2 / 3;
 
-  const referenceText = new MultiLineText( '', {
-    font: LeastSquaresRegressionConstants.REFERENCE_FONT,
-    align: 'left'
-  } );
-  const sourceText = new MultiLineText( '', { font: LeastSquaresRegressionConstants.SOURCE_FONT, align: 'left' } );
+    const self = this;
 
-  const children = [
-    referenceText,
-    sourceText
-  ];
+    const referenceText = new MultiLineText( '', {
+      font: LeastSquaresRegressionConstants.REFERENCE_FONT,
+      align: 'left'
+    } );
+    const sourceText = new MultiLineText( '', { font: LeastSquaresRegressionConstants.SOURCE_FONT, align: 'left' } );
 
-  // Create the content box
-  const content = new LayoutBox( { align: 'left', spacing: 10, children: children, maxWidth: maxContentWidth } );
+    const children = [
+      referenceText,
+      sourceText
+    ];
 
-  // Create the panel that contains the source and reference
-  const panel = new Panel( content, {
-    centerX: this.layoutBounds.centerX,
-    centerY: this.layoutBounds.centerY,
-    xMargin: 20,
-    yMargin: 20,
-    fill: 'white',
-    stroke: 'black'
-  } );
+    // Create the content box
+    const content = new LayoutBox( { align: 'left', spacing: 10, children: children, maxWidth: maxContentWidth } );
 
-  // Create the 'Closed Button" in the upper right corner with a circle and a cross inside it.
-  // The button is not hooked to any listener since the closing of this node is handled in the main screenView
-  const buttonSize = 15;
-  const buttonLineWidth = 2;
-  const circle = new Circle( buttonSize, {
-    fill: 'black',
-    stroke: 'white',
-    lineWidth: buttonLineWidth,
-    centerX: 0,
-    centerY: 0
-  } );
-  const l = buttonSize / 3;
-  const upSlopeLine = new Line( l, l, -l, -l, {
-    stroke: 'white',
-    lineWidth: buttonLineWidth,
-    centerX: 0,
-    centerY: 0
-  } );
-  const downwardSlopeLine = new Line( l, -l, -l, l, {
-    stroke: 'white',
-    lineWidth: buttonLineWidth,
-    centerX: 0,
-    centerY: 0
-  } );
-  const button = new Node( { children: [ circle, upSlopeLine, downwardSlopeLine ] } );
+    // Create the panel that contains the source and reference
+    const panel = new Panel( content, {
+      centerX: this.layoutBounds.centerX,
+      centerY: this.layoutBounds.centerY,
+      xMargin: 20,
+      yMargin: 20,
+      fill: 'white',
+      stroke: 'black'
+    } );
 
-  // Add a cursor when hovering (see https://github.com/phetsims/least-squares-regression/issues/10)
-  button.cursor = 'pointer';
+    // Create the 'Closed Button" in the upper right corner with a circle and a cross inside it.
+    // The button is not hooked to any listener since the closing of this node is handled in the main screenView
+    const buttonSize = 15;
+    const buttonLineWidth = 2;
+    const circle = new Circle( buttonSize, {
+      fill: 'black',
+      stroke: 'white',
+      lineWidth: buttonLineWidth,
+      centerX: 0,
+      centerY: 0
+    } );
+    const l = buttonSize / 3;
+    const upSlopeLine = new Line( l, l, -l, -l, {
+      stroke: 'white',
+      lineWidth: buttonLineWidth,
+      centerX: 0,
+      centerY: 0
+    } );
+    const downwardSlopeLine = new Line( l, -l, -l, l, {
+      stroke: 'white',
+      lineWidth: buttonLineWidth,
+      centerX: 0,
+      centerY: 0
+    } );
+    const button = new Node( { children: [ circle, upSlopeLine, downwardSlopeLine ] } );
 
-  // Add to this node
-  this.addChild( panel );
-  this.addChild( button );
+    // Add a cursor when hovering (see https://github.com/phetsims/least-squares-regression/issues/10)
+    button.cursor = 'pointer';
 
-  // Update the content of this node and the layout.
-  // no need to unlink, present for the lifetime of the sim
-  selectedDataSetProperty.link( function( selectedDataSet ) {
-    referenceText.text = selectedDataSet.reference;
-    const formattedSourceString = StringUtils.format( sourcePatternString, selectedDataSet.source );
-    sourceText.text = formattedSourceString;
-    panel.centerX = self.layoutBounds.centerX;
-    panel.centerY = self.layoutBounds.centerY;
-    button.centerX = panel.right;
-    button.centerY = panel.top;
-  } );
+    // Add to this node
+    this.addChild( panel );
+    this.addChild( button );
+
+    // Update the content of this node and the layout.
+    // no need to unlink, present for the lifetime of the sim
+    selectedDataSetProperty.link( function( selectedDataSet ) {
+      referenceText.text = selectedDataSet.reference;
+      const formattedSourceString = StringUtils.format( sourcePatternString, selectedDataSet.source );
+      sourceText.text = formattedSourceString;
+      panel.centerX = self.layoutBounds.centerX;
+      panel.centerY = self.layoutBounds.centerY;
+      button.centerX = panel.right;
+      button.centerY = panel.top;
+    } );
+  }
 }
 
 leastSquaresRegression.register( 'SourceAndReferenceNode', SourceAndReferenceNode );
-
-inherit( ScreenView, SourceAndReferenceNode );
 export default SourceAndReferenceNode;
