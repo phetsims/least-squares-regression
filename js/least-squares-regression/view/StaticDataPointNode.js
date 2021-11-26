67be6d160efb9f0ebdd3b1f47c66c2d6fc993d2c
phet-dev <phetadmin@gmail.com>
2021-11-26 03:18:38 -0700

    update copyright dates from daily grunt work

--- js/least-squares-regression/view/StaticDataPointNode.js
+++ js/least-squares-regression/view/StaticDataPointNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2021, University of Colorado Boulder
 
 /**
  * Type that represents a static dataPoint in the view.
--- js/least-squares-regression/view/SumOfSquaredResidualsChart.js
+++ js/least-squares-regression/view/SumOfSquaredResidualsChart.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2021, University of Colorado Boulder
 
 /**
  * A Scenery Node that represents a barometer chart of the sum of square residuals .
