f83875e7be6ddcb138db7d96b2a00ea4f69ad9d7
Sam Reid <reids@colorado.edu>
2022-05-07 09:33:25 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/leastSquaresRegressionStrings.ts
+++ js/leastSquaresRegressionStrings.ts
@@ -9,109 +9,109 @@ import leastSquaresRegression from './leastSquaresRegression.js';
 
 type StringsType = {
   'least-squares-regression': {
-    'title': string
-  },
-  'bestFitLine': string,
-  'myLine': string,
-  'residuals': string,
-  'squaredResiduals': string,
-  'sum': string,
-  'a': string,
-  'b': string,
+    'title': string;
+  };
+  'bestFitLine': string;
+  'myLine': string;
+  'residuals': string;
+  'squaredResiduals': string;
+  'sum': string;
+  'a': string;
+  'b': string;
   'symbol': {
-    'x': string,
-    'y': string,
-    'r': string
-  },
-  'questionMark': string,
-  'sourcePattern': string,
-  'correlationCoefficient': string,
+    'x': string;
+    'y': string;
+    'r': string;
+  };
+  'questionMark': string;
+  'sourcePattern': string;
+  'correlationCoefficient': string;
   'custom': {
-    'graphTitle': string
-  },
+    'graphTitle': string;
+  };
   'temperatureFahrenheitLatitude': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'temperatureFahrenheitLongitude': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'temperatureCelsiusLatitude': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'temperatureCelsiusLongitude': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'spendingSalary': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'wageYear': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'mortalityYear': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'userYear': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'gasolineYear': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'lifeTV': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'speedDistance': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'temperatureFahrenheitChirp': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'temperatureCelsiusChirp': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
-  },
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
+  };
   'heightShoe': {
-    'graphTitle': string,
-    'xAxisTitle': string,
-    'yAxisTitle': string,
-    'reference': string
+    'graphTitle': string;
+    'xAxisTitle': string;
+    'yAxisTitle': string;
+    'reference': string;
   }
 };
 
