955df7e709bf38b0af5ef6a9c458c669ee91d6cb
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:33 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/leastSquaresRegressionStrings.ts
+++ js/leastSquaresRegressionStrings.ts
@@ -5,113 +5,185 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import leastSquaresRegression from './leastSquaresRegression.js';
 
 type StringsType = {
   'least-squares-regression': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'bestFitLine': string;
+  'bestFitLineProperty': TReadOnlyProperty<string>;
   'myLine': string;
+  'myLineProperty': TReadOnlyProperty<string>;
   'residuals': string;
+  'residualsProperty': TReadOnlyProperty<string>;
   'squaredResiduals': string;
+  'squaredResidualsProperty': TReadOnlyProperty<string>;
   'sum': string;
+  'sumProperty': TReadOnlyProperty<string>;
   'a': string;
+  'aProperty': TReadOnlyProperty<string>;
   'b': string;
+  'bProperty': TReadOnlyProperty<string>;
   'symbol': {
     'x': string;
+    'xProperty': TReadOnlyProperty<string>;
     'y': string;
+    'yProperty': TReadOnlyProperty<string>;
     'r': string;
+    'rProperty': TReadOnlyProperty<string>;
   };
   'questionMark': string;
+  'questionMarkProperty': TReadOnlyProperty<string>;
   'sourcePattern': string;
+  'sourcePatternProperty': TReadOnlyProperty<string>;
   'correlationCoefficient': string;
+  'correlationCoefficientProperty': TReadOnlyProperty<string>;
   'custom': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
   };
   'temperatureFahrenheitLatitude': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'temperatureFahrenheitLongitude': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'temperatureCelsiusLatitude': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'temperatureCelsiusLongitude': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'spendingSalary': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'wageYear': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'mortalityYear': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'userYear': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'gasolineYear': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'lifeTV': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'speedDistance': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'temperatureFahrenheitChirp': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'temperatureCelsiusChirp': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   };
   'heightShoe': {
     'graphTitle': string;
+    'graphTitleProperty': TReadOnlyProperty<string>;
     'xAxisTitle': string;
+    'xAxisTitleProperty': TReadOnlyProperty<string>;
     'yAxisTitle': string;
+    'yAxisTitleProperty': TReadOnlyProperty<string>;
     'reference': string;
+    'referenceProperty': TReadOnlyProperty<string>;
   }
 };
 
