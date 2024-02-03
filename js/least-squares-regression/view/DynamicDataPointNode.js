41594cdd121dc90b583c232e0dcf43865d8b4a44
phet-dev <phetadmin@gmail.com>
2024-02-03 03:21:34 -0700

    update copyright dates from daily grunt work

--- js/least-squares-regression/view/DynamicDataPointNode.js
+++ js/least-squares-regression/view/DynamicDataPointNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * Type that represents a movable dataPoint in the view.
--- js/least-squares-regression/view/GraphNode.js
+++ js/least-squares-regression/view/GraphNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * View representation of a Graph. Responsible for the view of 'MyLine', 'BestFitLine'
