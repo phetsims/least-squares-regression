e1f12c579ed8468c1e51cf169de0bb50041db97a
phet-dev <phetadmin@gmail.com>
2025-01-03 04:01:54 -0700

    update copyright dates from daily grunt work

--- js/least-squares-regression/view/EquationNode.ts
+++ js/least-squares-regression/view/EquationNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2024, University of Colorado Boulder
+// Copyright 2014-2025, University of Colorado Boulder
 
 /**
  * Equation Node that renders a text node of a linear equation of the form y = m x + b where m and b are numerical values
